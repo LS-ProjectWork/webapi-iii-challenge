765d8fe493f3d5d0149b04b57ac2057d5d5d0287
dpayton23 <darien.payton71@gmail.com>
2019-04-24 19:00:04 -0400

    created middleware function and added text to homepage

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const server = express()
 
+function upperCase(req, res, next){
+    if(req.name == req.name.toUpperCase) {
+        next()
+    } else {
+        res.status(404).send('Name needs to be uppercased')
+    }
+}
+
+server.use('/', (req, res) => {
+    res.status(200).send('This is the home page')
+})
+
 server.listen(6000, () => {
     console.log('server is on port 6000')
 })
